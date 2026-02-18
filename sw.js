3763e040a27311ae8cde2805f34b8bc60c692973
Manuelpita90 <manuelpita90@gmail.com>
2026-02-18 18:11:14 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'lotto-ia-v2';
+const urlsToCache = [
+  './movil.html',
+  './manifest.json',
+  './img/icon.png'
+];
+
+// Instalación del Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Abriendo caché');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activación (Limpieza de cachés viejos si cambias la versión)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Estrategia de Caché: Stale-While-Revalidate
+// Carga instantáneamente desde caché y actualiza en segundo plano
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(event.request).then(response => {
+        const fetchPromise = fetch(event.request).then(networkResponse => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+        return response || fetchPromise;
+      });
+    })
+  );
+});
